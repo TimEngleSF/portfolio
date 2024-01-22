9756b3541b7b5322acb1bb8f00176132fd9b5c54
Tim Engle <timothy.englew@gmail.com>
2024-01-21 20:09:13 -0800

    Fix image position for cards @ md breakpoint

--- src/components/Projects/ProjectContainer.jsx
+++ src/components/Projects/ProjectContainer.jsx
@@ -8,18 +8,21 @@ const ProjectContainer = () => {
       title: "Leetcode Tracker",
       ghUrl: "https://github.com/TimEngleSF/leetcode-tracker",
       img: "/project_img/lc_tracker.jpg",
+      shiftImgY: "md:translate-y-8",
     },
     {
       url: "https://l.timengle.dev",
       title: "URL Shortener",
       ghUrl: "https://github.com/TimEngleSF/url-shortener-go",
       img: "/project_img/url_shortener.jpg",
+      shiftImgY: "",
     },
     {
       url: "https://4-7-8.netlify.app/",
       title: "4-7-8 Visualizer",
       ghUrl: "https://github.com/TimEngleSF/4-7-8-Breathing-Visualizer",
       img: "/project_img/478_visualizer.png",
+      shiftImgY: "md:translate-y-11",
     },
   ];
   const projectComponents = projectsData.map((project, i) => (
@@ -29,10 +32,11 @@ const ProjectContainer = () => {
       ghUrl={project.ghUrl}
       img={project.img}
       key={i}
+      shiftImgY={project.shiftImgY}
     />
   ));
   return (
-    <section className="mx-auto flex flex-col rounded-b-md pb-20 md:w-[80%] md:flex-row md:justify-center md:gap-12">
+    <section className="mx-auto flex flex-col items-center gap-12 rounded-b-md pb-20 md:w-[80%] md:justify-center lg:flex-row lg:gap-12">
       {projectComponents}
     </section>
   );
--- src/components/Projects/ProjectsCard.jsx
+++ src/components/Projects/ProjectsCard.jsx
@@ -11,9 +11,10 @@ const ProjectsCard = ({
   ghUrl,
   url,
   stackIcons,
+  shiftImgY,
 }) => {
   return (
-    <div className="card w-60 rounded-md bg-termScreen font-scp text-termTop shadow-xl">
+    <div className="card w-60 rounded-md bg-termScreen font-scp text-termTop shadow-xl  md:w-80 lg:w-60">
       <div className="h-6 w-full rounded-t-md bg-termTop">
         <div className="ml-2 flex h-full w-16 items-center justify-between">
           <div className={`h-4 w-4 rounded-full bg-termExit`}></div>
@@ -22,8 +23,8 @@ const ProjectsCard = ({
         </div>
       </div>
       <a href={url} target="_blank">
-        <figure className="max-h-[193.5px] overflow-hidden">
-          <img src={img} alt={alt} />
+        <figure className="max-h-[193.5px] overflow-hidden md:h-[193.5px] ">
+          <img src={img} alt={alt} className={`${shiftImgY}`} />
         </figure>
       </a>
       <div className="card-body items-center rounded-b-md bg-termScreen p-3 text-center">
