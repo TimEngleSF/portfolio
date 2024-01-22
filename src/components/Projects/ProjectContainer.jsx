8f7b4798f0ad4027cbd544e7b48ef5b674488046
Tim Engle <timothy.englew@gmail.com>
2024-01-21 17:57:43 -0800

    Add Projects Section

---
+++ src/components/Projects/ProjectContainer.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import ProjectsCard from "./ProjectsCard";
+
+const ProjectContainer = () => {
+  const projectsData = [
+    {
+      url: "https://www.npmjs.com/package/lc-tracker",
+      title: "Leetcode Tracker",
+      ghUrl: "https://github.com/TimEngleSF/leetcode-tracker",
+      img: "/project_img/lc_tracker.jpg",
+    },
+    {
+      url: "https://l.timengle.dev",
+      title: "URL Shortener",
+      ghUrl: "https://github.com/TimEngleSF/url-shortener-go",
+      img: "/project_img/url_shortener.jpg",
+    },
+    {
+      url: "https://4-7-8.netlify.app/",
+      title: "4-7-8 Visualizer",
+      ghUrl: "https://github.com/TimEngleSF/4-7-8-Breathing-Visualizer",
+      img: "/project_img/478_visualizer.png",
+    },
+  ];
+  const projectComponents = projectsData.map((project, i) => (
+    <ProjectsCard
+      title={project.title}
+      url={project.url}
+      ghUrl={project.ghUrl}
+      img={project.img}
+      key={i}
+    />
+  ));
+  return (
+    <section className="mx-auto flex flex-col md:w-[80%] md:flex-row md:justify-center md:gap-12">
+      {projectComponents}
+    </section>
+  );
+};
+
+export default ProjectContainer;
--- src/components/Projects/ProjectsCard.jsx
+++ src/components/Projects/ProjectsCard.jsx
@@ -13,7 +13,7 @@ const ProjectsCard = ({
   stackIcons,
 }) => {
   return (
-    <div className="card w-60 bg-termScreen shadow-xl font-scp rounded-md text-termTop">
+    <div className="card w-60 rounded-md bg-termScreen font-scp text-termTop shadow-xl">
       <div className="h-6 w-full rounded-t-md bg-termTop">
         <div className="ml-2 flex h-full w-16 items-center justify-between">
           <div className={`h-4 w-4 rounded-full bg-termExit`}></div>
@@ -22,19 +22,19 @@ const ProjectsCard = ({
         </div>
       </div>
       <a href={url} target="_blank">
-        <figure>
+        <figure className="max-h-[193.5px] overflow-hidden">
           <img src={img} alt={alt} />
         </figure>
       </a>
-      <div className="card-body items-center text-center p-3">
+      <div className="card-body items-center bg-termScreen p-3 text-center">
         <a href={url} target="_blank">
           <h2 className="card-title">{title}</h2>
         </a>
-        <div className="stack-icons flex gap-2">
+        {/* <div className="stack-icons flex gap-2">
           {stackIcons.map((Icon, index) => (
             <Icon key={index} size={"1.5rem"} />
           ))}
-        </div>
+        </div> */}
         <div className="card-actions">
           <a href={ghUrl}>
             <FaGithub size={"2rem"} />
