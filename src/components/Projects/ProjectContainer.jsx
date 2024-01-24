800e42ec6de6b9f7681286746d1a0d96f0260b1c
Tim Engle <timothy.englew@gmail.com>
2024-01-24 10:23:29 -0800

    Flex wrap project cards at md breakpoint

--- src/components/Projects/ProjectContainer.jsx
+++ src/components/Projects/ProjectContainer.jsx
@@ -8,7 +8,7 @@ const ProjectContainer = () => {
       title: "Leetcode Tracker",
       ghUrl: "https://github.com/TimEngleSF/leetcode-tracker",
       img: "/project_img/lc_tracker.jpg",
-      shiftImgY: "translate-y-8 lg:translate-y-0",
+      shiftImgY: "translate-y-8 md:translate-y-0",
     },
     {
       url: "https://l.timengle.dev",
@@ -22,7 +22,7 @@ const ProjectContainer = () => {
       title: "4-7-8 Visualizer",
       ghUrl: "https://github.com/TimEngleSF/4-7-8-Breathing-Visualizer",
       img: "/project_img/478_visualizer.png",
-      shiftImgY: "translate-y-11 lg:translate-y-0",
+      shiftImgY: "translate-y-11 md:translate-y-0",
     },
   ];
   const projectComponents = projectsData.map((project, i) => (
@@ -44,7 +44,7 @@ const ProjectContainer = () => {
         <h2 className="mb-6 border-b-4 border-termScreen pb-2 text-3xl font-bold">
           Projects
         </h2>
-        <div className="flex flex-col items-center gap-12 rounded-b-md pb-20 md:w-[80%] md:justify-center lg:flex-row lg:gap-12">
+        <div className="flex flex-col items-center gap-12 rounded-b-md pb-20 md:w-[80%] md:flex-row md:flex-wrap md:justify-center lg:gap-12">
           {projectComponents}
         </div>
       </section>
